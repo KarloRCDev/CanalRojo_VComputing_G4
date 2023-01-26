3c9c6d851a2a46cc5d87a67bc021974bb9a4a1f2
MARlon024 <marlondurand675@gmail.com>
2023-01-25 22:39:11 -0500

    gray_scale

---
+++ Tarea Lena-Image/gray_scale.py
@@ -0,0 +1,11 @@
+import cv2
+from pathlib import Path
+import numpy as np
+
+current_directory = Path(__file__).resolve().parent
+img = cv2.imread(f"{current_directory}/imagenes/Lenna.jpg")
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imwrite(f"{current_directory}/imagenes/gray_image_Lenna.jpg",gray_image)
+cv2.imshow("image",gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

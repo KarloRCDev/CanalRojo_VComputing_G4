2217d5d83d04066ec0b93cba59c15d4e1f0b068b
KarloRomero <karlo.romero@unmsm.edu.pe>
2023-01-28 16:56:39 -0500

    Corregir rotateimage

--- Tarea 2 Detect-Rotate/rotateimage.py
+++ Tarea 2 Detect-Rotate/rotateimage.py
@@ -3,35 +3,26 @@ from pathlib import Path
 import numpy as np
 
 current_directory = Path(__file__).resolve().parent
-'''path = f"{current_directory}/imagenesDPI/image_72dpi.jpg"
-path2 = f"{current_directory}/imagenesDPI"
+path = f"{current_directory}/imagenes/image.jpg"
+path2 = f"{current_directory}/imagenesRotated"
+path3 = f"{current_directory}/imagenesReflected"
 
 img = cv2.imread(path)
+cv2.imshow("Original", img)
+cv2.waitKey(0)
 
-# Rotate the image by 90 degrees clockwise
-img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+# Rotar la imagen 90 grados
+img_rotated = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
-# Save the rotated image
-cv2.imwrite(f"{path2}/rotated_image.jpg", img)
-cv2.imshow("Rotated", img)
+# Guardar y mostrar la imagen
+cv2.imwrite(f"{path2}/rotated_image.jpg", img_rotated)
+cv2.imshow("Rotated", img_rotated)
 cv2.waitKey(0)  # Espera a que el usuario presione una tecla
 
+#Reflejar la imagen
 img_reflected = cv2.flip(img, 1)
+
+# Guardar y mostrar la imagen
+cv2.imwrite(f"{path3}/rotated_image.jpg", img_reflected)
 cv2.imshow("Reflected", img_reflected)
 cv2.waitKey(0)  # Espera a que el usuario presione una tecla
-'''
-
-# Load an image
-image = cv2.imread(f"{current_directory}/imagenes/image.jpg")
-
-# Define the shear transformation matrix
-shear_matrix = np.float32([[1, 0.5, 0], [0, 1, 0]])
-
-# Apply the shear transformation
-sheared_image = cv2.warpAffine(
-    image, shear_matrix, (image.shape[1], image.shape[0]))
-
-# Display the image
-cv2.imshow("Sheared Image", sheared_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()

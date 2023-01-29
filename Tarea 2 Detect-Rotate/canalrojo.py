0ec508d35edc01e1b5fb033d16aa1d375fe1d949
KarloRomero <karlo.romero@unmsm.edu.pe>
2023-01-29 14:08:58 -0500

    Update canalrojo.py

--- Tarea 2 Detect-Rotate/canalrojo.py
+++ Tarea 2 Detect-Rotate/canalrojo.py
@@ -7,8 +7,7 @@ path = f"{current_directory}/imagenes/image.jpg"
 
 # Carga la imagen
 image = cv2.imread(path)
-b, g, r = cv2.split(image)
-# Crea una copia de la imagen en escala de grises
+b, g, r = cv2.split(image) # Separa la imagen en sus 3 canales blue, green, red en escala de grises
 
 
 # Crea una copia de la imagen solo con el canal rojo
@@ -19,7 +18,8 @@ red[:, :, 1] = 0
 # Muestra la imagen original
 cv2.imshow("Original", image)
 cv2.waitKey(0) # Espera a que el usuario presione una tecla
-cv2.imshow("Canal rojo 1", r)
+# Muestra el canal rojo de la imagen en escala de grises
+cv2.imshow("Canal rojo en escala de grises", r)
 cv2.waitKey(0) # Espera a que el usuario presione una tecla
 cv2.imshow("Canal rojo 2", red)
 cv2.waitKey(0)

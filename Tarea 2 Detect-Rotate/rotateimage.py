c0bd1251c364f549ef5dfa93ac6e1612fc3753c4
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2023-01-30 08:48:16 -0500

    change file name

--- Tarea 2 Detect-Rotate/rotateimage.py
+++ Tarea 2 Detect-Rotate/rotateimage.py
@@ -19,10 +19,10 @@ cv2.imwrite(f"{path2}/rotated_image.jpg", img_rotated)
 cv2.imshow("Rotated", img_rotated)
 cv2.waitKey(0)  # Espera a que el usuario presione una tecla
 
-#Reflejar la imagen
+# Reflejar la imagen
 img_reflected = cv2.flip(img, 1)
 
 # Guardar y mostrar la imagen
-cv2.imwrite(f"{path3}/rotated_image.jpg", img_reflected)
+cv2.imwrite(f"{path3}/reflected_image.jpg", img_reflected)
 cv2.imshow("Reflected", img_reflected)
 cv2.waitKey(0)  # Espera a que el usuario presione una tecla

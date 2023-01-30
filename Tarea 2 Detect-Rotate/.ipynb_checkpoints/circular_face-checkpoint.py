66db89d44701b3e0b54b6e1875c37d72a236ac62
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2023-01-29 19:45:20 -0500

    fix line path

--- Tarea 2 Detect-Rotate/.ipynb_checkpoints/circular_face-checkpoint.py
+++ Tarea 2 Detect-Rotate/.ipynb_checkpoints/circular_face-checkpoint.py
@@ -1,5 +1,4 @@
 import cv2
-import os
 from pathlib import Path
 import numpy as np
 # current_directory = Path(__file__).resolve().parent
@@ -23,21 +22,16 @@ img = cv2.imread(f"{current_directory}/imagenes/per.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Load the face detector
-
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
+face_cascade = cv2.CascadeClassifier("C:\Python311\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml")
 
 # Detect faces in the image
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
 
 # Iterate over the faces
 for (x, y, w, h) in faces:
     # Create a mask with a circular shape
     mask = np.zeros_like(img)
-    mask = cv2.circle(mask, (x + w//2, y + h//2),
-                      (w + h)//4, (255, 255, 255), -1)
+    mask = cv2.circle(mask, (x + w//2, y + h//2), (w + h)//4, (255, 255, 255), -1)
     # Apply the mask to the face
     img = cv2.addWeighted(img, 0.7, mask, 0.3, 0)
 
@@ -45,3 +39,4 @@ for (x, y, w, h) in faces:
 cv2.imshow("Face Masked", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+

eaf2e9b735977761d8b0ca8c3829e318e98fb434
MARlon024 <marlondurand675@gmail.com>
2023-02-15 15:02:02 -0500

    update

--- Implements two algorithm/circle_draw.py
+++ Implements two algorithm/circle_draw.py
@@ -1,113 +1,35 @@
-# import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+def draw_circle_bresenham(img, center, radius):
+    x, y = 0, radius
+    d = 3 - 2 * radius
+    while x <= y:
+        # Plot the eight octants of the circle
+        img[center[1] + x, center[0] + y] = (0, 0, 255)
+        img[center[1] + y, center[0] + x] = (0, 0, 255)
+        img[center[1] - x, center[0] + y] = (0, 0, 255)
+        img[center[1] - y, center[0] + x] = (0, 0, 255)
+        img[center[1] + x, center[0] - y] = (0, 0, 255)
+        img[center[1] + y, center[0] - x] = (0, 0, 255)
+        img[center[1] - x, center[0] - y] = (0, 0, 255)
+        img[center[1] - y, center[0] - x] = (0, 0, 255)
 
-# def draw_circle(xc, yc, r):
-#     x = r
-#     y = 0
-#     dx = 1
-#     dy = 1
-#     err = dx - (r << 1)
+        if d < 0:
+            d = d + 4 * x + 6
+        else:
+            d = d + 4 * (x - y) + 10
+            y = y - 1
+        x = x + 1
 
-#     # create empty lists to hold the x and y coordinates
-#     x_coords = []
-#     y_coords = []
+    # Draw a small filled circle at the center of the circle
+    cv2.circle(img, center, 3, (0, 255, 0), -1)
 
-#     while x >= y:
-#         # add the current point to the coordinate lists
-#         x_coords.append(xc + x)
-#         y_coords.append(yc + y)
-
-#         x_coords.append(xc + y)
-#         y_coords.append(yc + x)
-
-#         x_coords.append(xc - y)
-#         y_coords.append(yc + x)
-
-#         x_coords.append(xc - x)
-#         y_coords.append(yc + y)
-
-#         x_coords.append(xc - x)
-#         y_coords.append(yc - y)
-
-#         x_coords.append(xc - y)
-#         y_coords.append(yc - x)
-
-#         x_coords.append(xc + y)
-#         y_coords.append(yc - x)
-
-#         x_coords.append(xc + x)
-#         y_coords.append(yc - y)
-
-#         # calculate the error term
-#         if err <= 0:
-#             y += 1
-#             err += dy
-#             dy += 2
-#         if err > 0:
-#             x -= 1
-#             dx += 2
-#             err += dx - (r << 1)
-
-#     # plot the circle using the coordinate lists
-#     plt.plot(x_coords, y_coords, 'bo')
-#     plt.axis('equal')
-#     plt.show()
-
-# # example usage
-# draw_circle(50, 50, 30)
-
-import matplotlib.pyplot as plt
-
-def draw_circle(xc, yc, r):
-    x = r
-    y = 0
-    dx = 1
-    dy = 1
-    err = dx - (r << 1)
-
-    # create empty lists to hold the x and y coordinates
-    x_coords = []
-    y_coords = []
-
-    while x >= y:
-        # add the current point to the coordinate lists
-        x_coords.append(xc + x)
-        y_coords.append(yc + y)
-
-        x_coords.append(xc + y)
-        y_coords.append(yc + x)
-
-        x_coords.append(xc - y)
-        y_coords.append(yc + x)
-
-        x_coords.append(xc - x)
-        y_coords.append(yc + y)
-
-        x_coords.append(xc - x)
-        y_coords.append(yc - y)
-
-        x_coords.append(xc - y)
-        y_coords.append(yc - x)
-
-        x_coords.append(xc + y)
-        y_coords.append(yc - x)
-
-        x_coords.append(xc + x)
-        y_coords.append(yc - y)
-
-        # calculate the error term
-        if err <= 0:
-            y += 1
-            err += dy
-            dy += 2
-        if err > 0:
-            x -= 1
-            dx += 2
-            err += dx - (r << 1)
-
-    # plot the circle using the coordinate lists
-    plt.plot(x_coords, y_coords, 'bo')
-    plt.axis('equal')
-    plt.show()
+    cv2.imshow('Circle', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 # example usage
-draw_circle(50, 50, 30)
+img = np.zeros((500, 500, 3), np.uint8)
+center = (250, 250)
+radius = 100
+draw_circle_bresenham(img, center, radius)

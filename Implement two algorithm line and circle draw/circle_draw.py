00d2fc465763c9c0349649e489868a51d2b4b1cf
MARlon024 <marlondurand675@gmail.com>
2023-02-15 15:04:50 -0500

    update

--- Implement two algorithm line and circle draw/circle_draw.py
+++ Implement two algorithm line and circle draw/circle_draw.py
@@ -4,7 +4,7 @@ def draw_circle_bresenham(img, center, radius):
     x, y = 0, radius
     d = 3 - 2 * radius
     while x <= y:
-        # Plot the eight octants of the circle
+        
         img[center[1] + x, center[0] + y] = (0, 0, 255)
         img[center[1] + y, center[0] + x] = (0, 0, 255)
         img[center[1] - x, center[0] + y] = (0, 0, 255)
@@ -21,7 +21,6 @@ def draw_circle_bresenham(img, center, radius):
             y = y - 1
         x = x + 1
 
-    # Draw a small filled circle at the center of the circle
     cv2.circle(img, center, 3, (0, 255, 0), -1)
 
     cv2.imshow('Circle', img)

93c2e0dd9db77e42e74cef645c43906578a43e24
MARlon024 <marlondurand675@gmail.com>
2023-02-22 01:48:26 -0500

    up

--- Fractal Examples/tree.py
+++ Fractal Examples/tree.py
@@ -7,21 +7,21 @@ def draw_binary_tree(ax, x, y, length, angle, depth):
 
     x2, y2 = x + length * np.sin(np.deg2rad(angle)), y + length * np.cos(np.deg2rad(angle))
     ax.plot([x, x2], [y, y2], 'k-')
-    plt.pause(0.001) # Add a pause of 0.5 seconds to see how the tree is being built
+    plt.pause(0.001) 
     
     draw_binary_tree(ax, x2, y2, length*0.7, angle + 30, depth-1)
     draw_binary_tree(ax, x2, y2, length*0.7, angle - 30, depth-1)
 
 def main():
-    # Prompt user for depth of the binary tree
+    
     depth = int(input("Enter the depth of the binary tree: "))
 
-    # Create figure and axis objects
+    
     fig, ax = plt.subplots()
     ax.set_xlim([-200, 200])
     ax.set_ylim([-50, 250])
 
-    # Draw the binary tree
+    
     draw_binary_tree(ax, 0, 0, 80, 0, depth)
 
     plt.show()

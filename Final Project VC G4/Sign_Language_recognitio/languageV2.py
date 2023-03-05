d84be2e5e9def940959aba00f289047c3e90839b
KarloRomero <karlo.romero@unmsm.edu.pe>
2023-03-05 17:48:10 -0500

    sign language modification v2

--- Final Project VC G4/Sign_Language_recognitio/languageV2.py
+++ Final Project VC G4/Sign_Language_recognitio/languageV2.py
@@ -1,53 +1,53 @@
+# Importar las bibliotecas necesarias
 import cv2
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
 
+# Obtener la ruta del directorio actual
 current_directory = Path(__file__).resolve().parent
 
-# Load the cascade
-hand_cascade = cv2.CascadeClassifier(f'{current_directory}/haarcascade_hand.xml')
+# Cargar la cascada de Haar para la detección de manos
+hand_cascade = cv2.CascadeClassifier(f'{current_directory}/xml/haarcascade_hand.xml')
 
-# Load the pre-trained CNN model
-model = tf.keras.models.load_model(
-    f'{current_directory}/model_train/trained_model.h5')
+# Cargar el modelo pre-entrenado para la clasificación de letras
+model = tf.keras.models.load_model(f'{current_directory}/model_train/trained_model.h5')
 
-
-# Define the classes
+# Definir las clases (letras del alfabeto)
 classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
 
-# Load the image
+# Cargar la imagen a procesar
 img = cv2.imread(f'{current_directory}/imagen/A_mano.jpg', cv2.IMREAD_COLOR)
 
-# Convert the image to grayscale
+# Convertir la imagen a escala de grises
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect the hands
+# Detectar las manos en la imagen utilizando la cascada de Haar
 hands = hand_cascade.detectMultiScale(gray, 1.1, 4)
 
-# Draw the rectangle around each hand
+# Dibujar un rectángulo alrededor de cada mano y clasificarla en una letra
 for (x, y, w, h) in hands:
-    # Crop the hand region
+    # Recortar la región de interés (ROI) de la mano
     roi = gray[y:y+h, x:x+w]
-    # Resize the ROI to match the input shape of the model
+    # Redimensionar la ROI para que coincida con la forma de entrada del modelo
     roi = cv2.resize(roi, (48, 48))
-    # Convert the ROI to a 3D tensor
+    # Convertir la ROI a un tensor 3D
     roi = np.expand_dims(roi, axis=-1)
     roi = np.expand_dims(roi, axis=0)
-    # Predict the class of the hand
+    # Realizar la predicción de la letra correspondiente a la mano
     prediction = model.predict(roi)
-    # Get the class with highest probability
+    # Obtener la clase con la mayor probabilidad
     class_idx = np.argmax(prediction)
-    # Get the corresponding letter
+    # Obtener la letra correspondiente a la clase predicha
     letter = classes[class_idx]
-    # Draw the rectangle around the hand
+    # Dibujar un rectángulo alrededor de la mano
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Put the predicted letter on the image
+    # Colocar la letra predicha sobre la imagen
     cv2.putText(img, letter, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
-# Display the resulting image
+# Mostrar la imagen resultante con las manos detectadas y las letras clasificadas
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
-# Release resources
+# Liberar los recursos utilizados por la ventana de visualización
 cv2.destroyAllWindows()
--- Final Project VC G4/Sign_Language_recognitio/language_ROI_estatico.py
+++ Final Project VC G4/Sign_Language_recognitio/language_ROI_estatico.py
@@ -5,51 +5,51 @@ from pathlib import Path
 
 current_directory = Path(__file__).resolve().parent
 
-# Load the pre-trained CNN model
+# Cargar el modelo CNN pre-entrenado
 model = tf.keras.models.load_model(
     f'{current_directory}/model_train/trained_model.h5')
 
-# Create a dictionary to map class indices to letters
+# Crear un diccionario para mapear los índices de clase a las letras
 class_indices_to_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
 
-# Define the region of interest (ROI)
-roi = (200, 100, 300, 300)  # (x, y, width, height)
+# Definir la región de interés (ROI)
+roi = (200, 100, 300, 300)  # (x, y, ancho, alto)
 
-# Open the camera
+# Abrir la cámara
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Capture frame-by-frame
+    # Capturar el fotograma por fotograma
     ret, frame = cap.read()
 
-    # Draw a blue rectangle around the ROI
+    # Dibujar un rectángulo azul alrededor de la ROI
     x, y, w, h = roi
     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Get the pixels inside the ROI and resize the image
+    # Obtener los píxeles dentro de la ROI y cambiar el tamaño de la imagen
     roi_frame = frame[y:y+h, x:x+w]
     roi_frame_resized = cv2.resize(roi_frame, (48, 48))
 
-    # Preprocess the image
+    # Preprocesar la imagen
     roi_frame_resized = cv2.cvtColor(roi_frame_resized, cv2.COLOR_BGR2GRAY)
     roi_frame_resized = roi_frame_resized.reshape(1, 48, 48, 1) / 255.0
 
-    # Make a prediction using the model
+    # Realizar una predicción con el modelo
     prediction = model.predict(roi_frame_resized)
     class_index = np.argmax(prediction)
     letter = class_indices_to_letters[class_index]
 
-    # Display the letter on the frame
+    # Mostrar la letra en el fotograma
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, letter, (x, y-10), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-    # Display the resulting frame
-    cv2.imshow('Sign Language Recognition', frame)
+    # Mostrar el fotograma resultante
+    cv2.imshow('Reconocimiento de Lenguaje de Señas', frame)
 
-    # Exit if the user presses the 'q' key
+    # Salir si el usuario presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close the windows
+# Liberar la cámara y cerrar las ventanas
 cap.release()
 cv2.destroyAllWindows()
